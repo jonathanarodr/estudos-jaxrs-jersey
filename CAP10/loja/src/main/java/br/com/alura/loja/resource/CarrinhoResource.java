8a7053aa5d344c3e24a92dfb3502c8a42a80c6a5
jonathanarodr <jonathan.a.rodr@gmail.com>
2016-10-07 17:52:16 -0300

    Capítulo 10 -Utilizando filtros e logging

--- CAP10/loja/src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
+++ CAP10/loja/src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
@@ -7,7 +7,6 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -18,7 +17,6 @@ import com.thoughtworks.xstream.XStream;
 
 import br.com.alura.loja.dao.CarrinhoDAO;
 import br.com.alura.loja.modelo.Carrinho;
-import br.com.alura.loja.modelo.Produto;
 
 @Path("carrinhos")
 public class CarrinhoResource {
@@ -54,16 +52,6 @@ public class CarrinhoResource {
 	public Response remove(@PathParam("id") long id, @PathParam("produtoId") long produtoId) {
 		Carrinho carrinho = new CarrinhoDAO().busca(id);
 		carrinho.remove(produtoId);
-		
-		return Response.ok().build();
-	}
-	
-	@Path("{id}/produtos/{produtoId}")
-	@PUT
-	public Response altera(String conteudo, @PathParam("id") long id, @PathParam("produtoId") long produtoId) {
-		Carrinho carrinho = new CarrinhoDAO().busca(id);
-		Produto produto = (Produto) new XStream().fromXML(conteudo);
-		carrinho.troca(produto);
 		
 		return Response.ok().build();
 	}
---
+++ CAP10/loja/src/test/java/br/com/alura/loja/ClienteTestPOST.java
@@ -0,0 +1,54 @@
+package br.com.alura.loja;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import br.com.alura.loja.modelo.Carrinho;
+import br.com.alura.loja.modelo.Produto;
+
+public class ClienteTestPOST {
+	
+	private HttpServer server;
+
+	@Before
+	public void before() {
+	    this.server = Servidor.inicializaServidor();
+	}
+
+    @After
+    public void mataServidor() {
+        this.server.stop();
+    }
+	
+	@Test
+	public void testaQueAConexaoComOServidorFunciona() {
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target("http://localhost:8080");
+		
+		Carrinho carrinho = new Carrinho();
+        carrinho.adiciona(new Produto(314L, "Tablet", 999, 1));
+        carrinho.setRua("Rua Vergueiro");
+        carrinho.setCidade("Sao Paulo");
+        String xml = carrinho.toXML();
+        Entity<String> entity = Entity.entity(xml, MediaType.APPLICATION_XML);
+        
+        Response response = target.path("/carrinhos").request().post(entity);
+        Assert.assertEquals(201, response.getStatus());
+        
+        String location = response.getHeaderString("location");
+        String conteudo = client.target(location).request().get(String.class);
+        
+        Assert.assertTrue(conteudo.contains("Tablet"));
+    }
+
+}
